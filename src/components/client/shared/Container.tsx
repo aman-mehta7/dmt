abf440741d692dcfbf4c0921eae9a27c3a578f73
Aman-Mehta69 <uncutlyric@gmail.com>
2025-04-01 20:46:00 +0545

    staysDetails

--- src/components/client/shared/Container.tsx
+++ src/components/client/shared/Container.tsx
@@ -11,6 +11,8 @@ const Container: FC<Props> = ({ className, children }) => {
       className={`relative px-4 md:max-w-5xl lg:max-w-5xl xl:max-w-[100rem] mx-auto w-full h-auto ${className}`}
     >
       {children}
+      {/* <a href=""><img src="https://dmttourism.com/storage/css/thumbnail_images/UD3CmjAMxqYJNjVRs0260yfyiwWYUOAg0AOSStjb.jpg" alt="" className="
+     l  " /></a> */}
     </div>
   );
 };
